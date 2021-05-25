e1ba44965abcaae5fd4baf9cfdfe46d57aeb7a31
dalyIsaac <isaac.daly@outlook.com>
2021-05-25 23:04:16 +1200

    Added headers to all files

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+##
+## COMP90024 Cluster and Cloud Computing
+## Group 5
+## Aleksandar Pasquini (912504)
+## Amelia Fleischer-Boermans (389511)
+## Isaac Daly (1129173)
+## Mahardini Rizky Putri (921790)
+## Richard Yang (1215150)
+##
+
 
 # Build frontend
 cd frontend
--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
+##
+## COMP90024 Cluster and Cloud Computing
+## Group 5
+## Aleksandar Pasquini (912504)
+## Amelia Fleischer-Boermans (389511)
+## Isaac Daly (1129173)
+## Mahardini Rizky Putri (921790)
+## Richard Yang (1215150)
+##
 node backend/app.js
